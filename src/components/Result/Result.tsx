0fa2260fb1680b798b73fdcbefa53a561abaf63b
Emma Ramirez <ramirez.emma.g@gmail.com>
2017-09-10 08:30:19 -0600

    add Result component

---
+++ src/components/Result/Result.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+export class Result extends React.Component<{}, {}> {
+  constructor(props) {
+    super(props);
+  }
+
+  public render() {
+    return <div className='result'>
+      Result.
+    </div>;
+  }
+}
