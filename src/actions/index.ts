381b1d8991cc5f938b88ba7f108d4782b50cf1d9
Emma Ramirez <ramirez.emma.g@gmail.com>
2017-09-12 03:17:00 -0600

    add selectPokemon action

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -3,3 +3,5 @@ export { saveNuzlocke, SAVE_NUZLOCKE } from './saveNuzlocke';
 export { addPokemon, ADD_POKEMON } from './addPokemon';
 export { deletePokemon, DELETE_POKEMON } from './deletePokemon';
 export { editPokemon, EDIT_POKEMON } from './editPokemon';
+export { editBox, EDIT_BOX } from './editBox';
+export { selectPokemon, SELECT_POKEMON } from './selectPokemon';
