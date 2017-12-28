936d38130902d9184d2c92b9ee4cda278014f259
Emma Ramirez <ramirez.emma.g@gmail.com>
2017-12-28 14:30:26 -0700

    remove next, add real server code

--- server/dto/create-cat.dto.ts
+++
@@ -1,5 +0,0 @@
-export class createCatDto {
-  readonly name: string;
-  readonly age: number;
-  readonly breed: string;
-}
---
+++ server/index.js
@@ -0,0 +1,6 @@
+// @ts-check
+const Hapi = require('hapi');
+
+const server = Hapi.server({
+    port: '8080'
+});
