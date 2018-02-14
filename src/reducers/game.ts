3c3c0642cfe43dab25c41d8ff3806986f9bf7f04
Emma Ramirez <ramirez.emma.g@gmail.com>
2018-02-14 13:01:09 -0700

    make default game name Red

--- src/reducers/game.ts
+++ src/reducers/game.ts
@@ -1,6 +1,8 @@
 import { Action, EDIT_GAME, REPLACE_STATE } from '../actions';
 
-export function game(state = {}, action: Action<EDIT_GAME | REPLACE_STATE>) {
+export function game(state = {
+    name: 'Red'
+}, action: Action<EDIT_GAME | REPLACE_STATE>) {
     switch (action.type) {
         case 'EDIT_GAME':
             return { ...state, ...action.edit };
