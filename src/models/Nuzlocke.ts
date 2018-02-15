677887641e01bb0d74e30ecd47f61b0efad9ac06
Emma Ramirez <ramirez.emma.g@gmail.com>
2018-02-15 02:14:00 -0700

    flesh out NuzlockSave

--- src/models/Nuzlocke.ts
+++ src/models/Nuzlocke.ts
@@ -1,3 +1,18 @@
-export interface Nuzlocke {
-    
+import { Pokemon, Trainer } from 'models';
+
+
+export interface NuzlockeSave {
+    /** uuid */
+    id: string;
+    data: {
+        pokemon?: Pokemon[];
+        trainer?: Trainer;
+        style?: any;
+        game?: any;
+        box?: any;
+        rules?: any;
+    }
 }
+
+export interface Nuzlocke {
+    saves: NuzlockeSave[];}
