a7b0a4fafe670e42a63f0ba07794eb28bd0a8a00
EmmaRamirez <ramirez.emma.g@gmail.com>
2018-07-13 13:22:26 -0600

    fix: test for Autocomplete

--- src/components/Shared/__tests__/Autocomplete.test.tsx
+++ src/components/Shared/__tests__/Autocomplete.test.tsx
@@ -1,11 +1,15 @@
 import * as React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import { Autocomplete } from '..';
 
 describe('<Autcomplete />', () => {
     it('renders its contents', () => {
-        const wrapper = shallow(<Autocomplete value={'test'} onChange={() => {}} items={['test', 'test2', 'test-2']} />);
+        const component = <Autocomplete value={'test'} onChange={() => {}} items={['test', 'test2', 'test-2']} />;
+        const wrapper = shallow(component);
         expect(wrapper.is('.autocomplete')).toBe(true);
-        expect(wrapper.contains('.autocomplete-items')).toBe(true);
+        const mountedWrapper = mount(component);
+        mountedWrapper.setState({ isOpen: true, visibleItems: ['test', 'test3'] });
+        expect(mountedWrapper.find('.autocomplete-items').length).toBe(1);
+        expect(mountedWrapper.find('[role="item"]').length).toBe(2);
     });
 });
