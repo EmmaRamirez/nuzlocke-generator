77613d0ddf2462c8fda4f2579a97e18229dd782e
EmmaRamirez <ramirez.emma.g@gmail.com>
2018-07-13 13:01:29 -0600

    design: ad I-K pokemon images

--- src/components/Shared/__tests__/Autocomplete.test.tsx
+++ src/components/Shared/__tests__/Autocomplete.test.tsx
@@ -6,5 +6,6 @@ describe('<Autcomplete />', () => {
     it('renders its contents', () => {
         const wrapper = shallow(<Autocomplete value={'test'} onChange={() => {}} items={['test', 'test2', 'test-2']} />);
         expect(wrapper.is('.autocomplete')).toBe(true);
+        expect(wrapper.contains('.autocomplete-items')).toBe(true);
     });
 });
