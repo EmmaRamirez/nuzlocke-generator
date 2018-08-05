f256c9a88080b5f6a75e4d815ce2227405bd8d37
EmmaRamirez <ramirez.emma.g@gmail.com>
2018-08-05 13:47:19 -0600

    feat: add ThemeEditor component

--- src/components/ThemeEditor/ThemeEditor.tsx
+++ src/components/ThemeEditor/ThemeEditor.tsx
@@ -1,12 +1,30 @@
 import * as React from 'react';
+import { connect } from 'react-redux';
+import { cx } from 'emotion';
+
+import * as Styles from './styles';
 
 // tslint:disable-next-line:no-empty-interfaces
 export interface ThemeEditorProps {
 
 }
 
-export class ThemeEditor extends React.Component<ThemeEditorProps> {
+export class ThemeEditorBase extends React.Component<ThemeEditorProps> {
     public render() {
-        return <div />;
+        return (
+            <>
+                <div className={cx(Styles.sidebar)}>
+
+                </div>
+                <div className={''}>
+
+                </div>
+            </>
+        );
     }
 }
+
+export const ThemeEditor = connect(
+    null,
+    null,
+)(ThemeEditorBase);
---
+++ src/components/ThemeEditor/styles.ts
@@ -0,0 +1,5 @@
+import { css } from 'emotion';
+
+export const sidebar = css`
+    width: 30%;
+`;
