4ab101c878cd747fa712c27c6ccc8db872179c22
EmmaRamirez <ramirez.emma.g@gmail.com>
2018-08-11 20:42:43 -0600

    refactor: add more props to ChampsPokemon

--- src/components/ChampsPokemon/ChampsPokemon.tsx
+++ src/components/ChampsPokemon/ChampsPokemon.tsx
@@ -3,28 +3,63 @@ import * as React from 'react';
 import { PokemonIcon } from 'components/PokemonIcon';
 import { gameOfOriginToColor, Game } from 'utils';
 import { Pokemon } from 'models';
+import { GenderElement, GenderElementProps } from '../Shared';
+import { css, cx } from 'emotion';
 
-export type ChampsPokemonProps = { [P in keyof Pokemon]?: any };
+export const champsPokemon = (options) => css`
+    height: ${options.height};
+    width: ${options.width};
+    margin: ${options.margin};
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    background: ${options.background};
+    padding: .25rem;
+    cursor: pointer;
+`;
+
+export type ChampsPokemonProps = {
+    [P in keyof Pokemon]?: any;
+} & {
+    showNickname?: boolean;
+    showGender?: boolean;
+    showLevel?: boolean;
+};
 
 export class ChampsPokemon extends React.Component<ChampsPokemonProps> {
     constructor(props) {
         super(props);
     }
 
+    private static defaultProps = {
+        showNickname: true,
+        showGender: true,
+        showLevel: true,
+    };
+
+    private getWidth = () => {
+        const base = 48;
+        return base +
+            (this.props.showGender ? 24 : 0) +
+            (this.props.showNickname ? 128 : 0) +
+            (this.props.showLevel ? 24 : 0);
+    }
+
     public render() {
         return (
             <div
-                className='champs-pokemon'
-                style={{
-                    height: '48px',
-                    width: '48px',
-                    display: 'inline-flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    background: gameOfOriginToColor(this.props.gameOfOrigin),
-                    cursor: 'pointer',
-                }}>
+                className={cx(
+                    champsPokemon({
+                        background: gameOfOriginToColor(this.props.gameOfOrigin),
+                        height: '48px',
+                        width: this.getWidth(),
+                        margin: '4px',
+                    })
+                )}>
                 <PokemonIcon {...this.props as any} />
+                { this.props.showNickname && this.props.nickname }
+                { this.props.showGender && GenderElement(this.props.gender) }
+                { this.props.showLevel && ` Lv ${this.props.level}` }
             </div>
         );
     }
