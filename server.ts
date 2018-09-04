dd2bb500fb3eaea592054abd9ce0255d068e7bd6
EmmaRamirez <ramirez.emma.g@gmail.com>
2018-09-03 20:05:28 -0600

    feat: preliminary func for image api

---
+++ server.ts
@@ -0,0 +1,10 @@
+const request = require('request');
+
+const auth = { user: 'user-id', pass: 'api-key' };
+
+function getImage(html, css) {
+    const data = { html: html, css: css };
+    request.post({ url: 'https://hcti.io/v1/image', form: data, auth: auth }, (err, httpResponse, body) => {
+        return body;
+    });
+}
