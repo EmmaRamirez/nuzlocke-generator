8be9baa4ed30c1f441fea4b6d39c444c228c2838
EmmaRamirez <ramirez.emma.g@gmail.com>
2018-09-07 00:16:37 -0600

    fix: getBackgroundGradient types

--- src/utils/getBackgroundGradient.ts
+++ src/utils/getBackgroundGradient.ts
@@ -1,6 +1,7 @@
 import { typeToColor } from './typeToColor';
+import { Types } from './Types';
 
-export const getBackgroundGradient = (typeA: string, typeB: string): string => {
+export const getBackgroundGradient = (typeA: keyof typeof Types, typeB: keyof typeof Types): string => {
     if (typeB == null) {
         if (typeA == null) {
             return 'transparent';
