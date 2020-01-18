2dc7e40293aeb8f469b62c545cac30064303f305
Emma Ramirez <ramirez.emma.g@gmail.com>
2020-01-17 22:51:16 -0700

    wip

--- src/components/StatsEditor/StatsEditor.tsx
+++ src/components/StatsEditor/StatsEditor.tsx
@@ -3,6 +3,7 @@ import { BaseEditor } from 'components/BaseEditor';
 import { Pokemon } from 'models';
 import { connect } from 'react-redux';
 import { State } from 'state';
+import { Checkbox, Button, Intent } from '@blueprintjs/core';
 
 export interface StatsEditorProps {
     pokemon: Pokemon[];
@@ -11,7 +12,20 @@ export interface StatsEditorProps {
 export class StatsEditorBase extends React.Component<StatsEditorProps> {
     public render() {
         return <BaseEditor name='Stats'>
+            <ul>
+                <li>
+                    <Checkbox
+                        checked={true}
+                        name='averageLevel'
+                        label='Average Level'
+                    />
+                </li>
 
+                <li>
+                    <input type='text' name='' value='' />
+                    <Button intent={Intent.PRIMARY} icon='plus'></Button>
+                </li>
+            </ul>
         </BaseEditor>;
     }
 }
