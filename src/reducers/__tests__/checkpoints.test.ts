ddfe6989b51d83d0d0a1e892c8040b30fc9b1ae9
Emma Ramirez <ramirez.emma.g@gmail.com>
2020-05-20 06:13:27 -0600

    test: update checkpoint test

--- src/reducers/__tests__/checkpoints.test.ts
+++ src/reducers/__tests__/checkpoints.test.ts
@@ -9,18 +9,18 @@ describe('checkpoints', () => {
     it('works with add', () => {
         const state1 = genState();
         const subject = checkpoints(state1, addCustomCheckpoint({ name: 'TestBadge2', image: 'neat' }));
-        expect(subject).toBe(2);
+        expect(subject.length).toBe(2);
     });
 
     it('works with delete', () => {
         const state2 = genState();
         const subject = checkpoints(state2, deleteCheckpoint('TestBadge'));
-        expect(subject).toBe(0);
+        expect(subject.length).toBe(0);
     });
 
     it('works with edit', () => {
         const state3 = genState();
         const subject = checkpoints(state3, editCheckpoint({ image: 'test' }, 'TestBadge'));
-        expect(subject).toBe('test');
+        expect(subject[0].image).toBe('test');
     });
 });
