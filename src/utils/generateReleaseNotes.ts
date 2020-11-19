e32c967dca638ac524880c7bc77ff67c7beb1fdf
EmmaRamirez <ramirez.emma.g@gmail.com>
2020-11-19 16:27:26 -0700

    fix: fix autocomplete inputs

--- src/utils/generateReleaseNotes.ts
+++ src/utils/generateReleaseNotes.ts
@@ -698,5 +698,14 @@ Thank you!
 - External Cors-blocked images cannot be downloaded, however you can use a [base 64 converter](https://www.base64-image.de/) to bypass this
 - The 'Evolve' button is only available for Gen I & II games
 - **Drag and drop will not reorder positions, you will still have to do that in the editor**
+`,
+    '1.4.9': `
+## Fixes
+- Fixes to autocomplete inputs
+
+## Known Issues
+- External Cors-blocked images cannot be downloaded, however you can use a [base 64 converter](https://www.base64-image.de/) to bypass this
+- The 'Evolve' button is only available for Gen I & II games
+- **Drag and drop will not reorder positions, you will still have to do that in the editor**
 `
 };
