61057c2cfd3107d826a3cd3e82e34de3926383f9
EmmaRamirez <ramirez.emma.g@gmail.com>
2020-11-22 17:07:58 -0700

    chore: remove double-click event

--- src/components/AddPokemonButton/AddPokemonButton.tsx
+++ src/components/AddPokemonButton/AddPokemonButton.tsx
@@ -3,8 +3,6 @@ import { useDispatch } from 'react-redux';
 import { addPokemon, selectPokemon } from 'actions';
 import { Button, Intent } from '@blueprintjs/core';
 import { Pokemon } from 'models';
-import { isLocal, listOfPokemon, generateEmptyPokemon } from 'utils';
-import { take } from 'ramda';
 
 export function AddPokemonButton ({ pokemon }: {pokemon: Pokemon}) {
     const dispatch = useDispatch();
@@ -14,20 +12,11 @@ export function AddPokemonButton ({ pokemon }: {pokemon: Pokemon}) {
         dispatch(selectPokemon(pokemon.id));
     };
 
-    const onDoubleClick = () => {
-        if (isLocal()) {
-            listOfPokemon.forEach(species => {
-                dispatch(addPokemon(generateEmptyPokemon(undefined, {species})));
-            });
-        }
-    };
-
     return <Button
         icon="add"
         intent={Intent.SUCCESS}
         className="add-new-pokemon"
         onClick={onClick}
-        onDoubleClick={onDoubleClick}
     >
     Add New Pokémon
     </Button>;
