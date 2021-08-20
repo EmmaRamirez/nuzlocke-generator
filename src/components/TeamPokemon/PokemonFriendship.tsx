836bdfa9ebb61f6b72206f2956f92dca9b37c4bb
EmmaRamirez <ramirez.emma.g@gmail.com>
2021-08-20 14:06:29 -0700

    feat(TeamPokemon): add friendship display component

--- src/components/TeamPokemon/PokemonFriendship.tsx
+++ src/components/TeamPokemon/PokemonFriendship.tsx
@@ -4,3 +4,33 @@
 // "It's quite cute."	100-149
 // "You should treat it better. It's not used to you."	50-99
 // "It doesn't seem to like you at all. It looks mean."	0-49
+
+import * as React from 'react';
+import { Icon } from '@blueprintjs/core';
+const uuid = require('uuid');
+
+export const determineNumberOfHearts = (friendship: number) => {
+    if (friendship < 99 && friendship >= 50) return 1;
+    if (friendship < 149 && friendship >= 100) return 2;
+    if (friendship < 199 && friendship >= 150) return 3;
+    if (friendship < 200 && friendship >= 249) return 4;
+    if (friendship >= 250) return 5;
+    else return 0;
+};
+
+export const generateHearts = (friendship: ReturnType<typeof determineNumberOfHearts>) => {
+    return Array.from(Array(friendship).keys()).map(k => <Icon data-testid='friendship-icon' key={uuid()} icon='heart' />);
+};
+
+export interface PokemonFriendshipProps {
+    friendship?: number;
+}
+
+export function PokemonFriendship({ friendship }: PokemonFriendshipProps) {
+    if (!friendship) return null;
+    const numberOfHearts = determineNumberOfHearts(friendship);
+
+    if (numberOfHearts === 0) return <Icon data-testid='friendship-broken-icon' icon='heart-broken' />;
+
+    return <>{generateHearts(numberOfHearts)}</>;
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,7 @@
 export type onClick = (
     e?: React.SyntheticEvent<MouseEvent> | React.SyntheticEvent<HTMLButtonElement>,
 ) => void;
+
+export type Concrete<Type> = {
+    [Property in keyof Type]-?: Type[Property];
+};
